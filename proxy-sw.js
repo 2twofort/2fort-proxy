a05b7385f14493b04c2b0d4b4bd13348b2806f55
2Fort <svatoslavmelnik12035@gmail.com>
2026-03-07 00:32:23 -0800

    Update proxy-sw.js

--- proxy-sw.js
+++ proxy-sw.js
@@ -1,7 +1,10 @@
-// v4
+// v5
 importScripts('/2fort-proxy/uv/uv.bundle.js');
 importScripts('/2fort-proxy/uv/uv.config.js');
 importScripts('/2fort-proxy/uv/uv.sw.js');
 
 self.addEventListener('install',  () => self.skipWaiting());
 self.addEventListener('activate', e  => e.waitUntil(self.clients.claim()));
+
+const sw = new UVServiceWorker();
+self.addEventListener('fetch', e => sw.route(e));
