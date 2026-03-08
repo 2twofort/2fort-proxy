79c57adcdcd48adb404aa97fd60c24ff4c18e1a2
2Fort <svatoslavmelnik12035@gmail.com>
2026-03-08 16:02:46 -0700

    Update proxy-sw.js

--- proxy-sw.js
+++ proxy-sw.js
@@ -6,14 +6,27 @@ const scramjet = new ScramjetServiceWorker();
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
 
+async function wipeIDB() {
+  await new Promise(r => {
+    const req = indexedDB.deleteDatabase('$scramjet');
+    req.onsuccess = req.onerror = req.onblocked = r;
+  });
+}
+
 async function handleRequest(event) {
   try {
     await scramjet.loadConfig();
   } catch (e) {
+    await wipeIDB();
     return fetch(event.request);
   }
   if (scramjet.route(event)) {
-    return scramjet.fetch(event);
+    try {
+      return await scramjet.fetch(event);
+    } catch (e) {
+      await wipeIDB();
+      return fetch(event.request);
+    }
   }
   return fetch(event.request);
 }
