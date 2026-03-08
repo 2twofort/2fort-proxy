9fed5d354da9aad3826481e061333c10a0cee26c
2Fort <svatoslavmelnik12035@gmail.com>
2026-03-07 17:10:36 -0800

    Update register-sw.js

--- static/js/register-sw.js
+++ static/js/register-sw.js
@@ -2,10 +2,7 @@
 (async function () {
   if (!('serviceWorker' in navigator)) return;
   try {
-    const reg = await navigator.serviceWorker.register('/2fort-proxy/proxy-sw.js', {
-      scope: '/2fort-proxy/',
-      type: 'module'
-    });
+    const reg = await navigator.serviceWorker.register('/2fort-proxy/proxy-sw.js', { scope: '/2fort-proxy/' });
     console.log('[2Fort] SW registered:', reg.scope);
     if (!navigator.serviceWorker.controller) {
       navigator.serviceWorker.addEventListener('controllerchange', () => location.reload());
