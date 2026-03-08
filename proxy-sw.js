e6b75a64990a3fa433ce16d63fa86dac27ac0d78
2Fort <svatoslavmelnik12035@gmail.com>
2026-03-07 17:07:40 -0800

    Update proxy-sw.js

--- proxy-sw.js
+++ proxy-sw.js
@@ -1,4 +1,16 @@
-import '/2fort-proxy/scramjet/scramjet.all.js';
+importScripts('/2fort-proxy/scramjet/scramjet.all.js');
+
+const { ScramjetServiceWorker } = $scramjetLoadWorker();
+const scramjet = new ScramjetServiceWorker();
 
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    await scramjet.loadConfig();
+    if (scramjet.route(event)) {
+      return scramjet.fetch(event);
+    }
+    return fetch(event.request);
+  })());
+});
